091c8edd6f267e2f83f048c08133166f097b8daa
hardik arora <hardikarora5520@gmail.com>
2020-08-30 15:05:50 +0530

    message5

--- app.js
+++ app.js
@@ -29,19 +29,6 @@ app.use(
 app.use('/api/note', notesRoute ); 
 app.use('/api/user', userRoute);
 
-app.use((req, res, next) => {
-    const error = new HttpError('Could not find this route ', 404);
-    throw  error;
-})
-app.use((error, req, res, next)=> {
-    if(res.headerSent) {
-        return next(error);
-    }
-    res.status(error.code || 500);
-    res.json({message : error.message || "An unknown error occured"});
-});
-
-
 // mongoose.connect(process.env.db)
 // .then(() => { app.listen(8000);})
 // .catch( err => { console.log(err)});
